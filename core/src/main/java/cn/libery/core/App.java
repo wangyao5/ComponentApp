cdb2eddf2a35f8c1a4fda755c6ce1fe2715284f2
Libery <szq921618920@gmail.com>
2016-12-22 13:42:05 +0800

    update example

--- core/src/main/java/cn/libery/core/App.java
+++ core/src/main/java/cn/libery/core/App.java
@@ -1,12 +1,15 @@
 package cn.libery.core;
 
 import android.app.Application;
+import android.content.Context;
 import android.net.Uri;
 import android.util.Log;
 
 import com.lzh.nonview.router.Router;
 import com.lzh.nonview.router.exception.NotFoundException;
+import com.lzh.nonview.router.route.ActivityRouteBundleExtras;
 import com.lzh.nonview.router.route.RouteCallback;
+import com.lzh.nonview.router.route.RouteInterceptor;
 
 /**
  * Created by Libery on 2016/12/16.
@@ -34,6 +37,18 @@ public class App extends Application {
             @Override
             public void onOpenFailed(Uri uri, Exception e) {
                 Log.e("App", e.getMessage());
+            }
+        });
+        //拦截做是否登录校验
+        Router.setGlobalRouteInterceptor(new RouteInterceptor() {
+            @Override
+            public boolean intercept(final Uri uri, final ActivityRouteBundleExtras extras, final Context context) {
+                return false;
+            }
+
+            @Override
+            public void onIntercepted(final Uri uri, final ActivityRouteBundleExtras extras, final Context context) {
+
             }
         });
 
